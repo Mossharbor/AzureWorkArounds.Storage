06ca768434ff0a041af71e94c00ee3c1cbbe3e77
Matthew Johnson <mattj@mossharbor.com>
2018-05-15 20:12:09 -0700

    Fixed misspelling of mossharbor, added build files

--- Mossharbor.AzureWorkArounds.Storage/QueueExtensions.cs
+++ Mossharbor.AzureWorkArounds.Storage/QueueExtensions.cs
@@ -3,7 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 
-namespace Mosshaarbor.AzureWorkArounds.Storage
+namespace Mossharbor.AzureWorkArounds.Storage
 {
     public static class QueueExtensions
     {
